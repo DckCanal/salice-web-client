31056f85b919d0137955e74b817682c7132d6f03
Marco De Canal <marco.decanal@gmail.com>
2022-04-21 13:51:47 +0200

    click event listener applied to IconButton

--- components/DarkThemeToggler.js
+++ components/DarkThemeToggler.js
@@ -3,12 +3,8 @@ import NightlightIcon from "@mui/icons-material/Nightlight";
 import IconButton from "@mui/material/IconButton";
 export default function DarkThemeToggler({ onClick, isLight }) {
   return (
-    <IconButton sx={{ ml: 5 }}>
-      {isLight ? (
-        <NightlightIcon onClick={onClick} />
-      ) : (
-        <LightModeIcon onClick={onClick} />
-      )}
+    <IconButton sx={{ ml: 5 }} onClick={onClick}>
+      {isLight ? <NightlightIcon /> : <LightModeIcon />}
     </IconButton>
   );
 }
