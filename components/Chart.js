fce4e8232a1e9c5c5d06085b17a88e97e5f18815
Marco De Canal <marco.decanal@gmail.com>
2022-04-27 12:40:02 +0200

    code leaning on Chart.js

--- components/Chart.js
+++ components/Chart.js
@@ -8,28 +8,10 @@ import {
   Label,
   ResponsiveContainer,
   Tooltip,
-  CartesianGrid,
   Legend,
 } from "recharts";
 import Title from "./Title";
-import { getMonths, getShortMonths } from "../lib/dateTranslator";
-
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
-}
-
-const data = [
-  createData("00:00", 0),
-  createData("03:00", 300),
-  createData("06:00", 600),
-  createData("09:00", 800),
-  createData("12:00", 1500),
-  createData("15:00", 2000),
-  createData("18:00", 2400),
-  createData("21:00", 2400),
-  createData("24:00", undefined),
-];
+import { getShortMonths } from "../lib/dateTranslator";
 
 export default function Chart({
   lightTheme,
@@ -53,7 +35,7 @@ export default function Chart({
     <React.Fragment>
       {console.log(data)}
       <Title>Today</Title>
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width="99%">
         <LineChart
           data={data}
           margin={{
@@ -88,7 +70,6 @@ export default function Chart({
             contentStyle={{ backgroundColor: lightTheme ? "#ddd" : "#555" }}
           />
           <Legend />
-          {/* <CartesianGrid stroke="#f5f5f5" /> */}
           <Line
             isAnimationActive={true}
             type="monotone"
