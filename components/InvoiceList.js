9f1d580661277e47daca05788d07b9f0d0fcc473
Marco De Canal <marco.decanal@gmail.com>
2022-04-28 21:06:46 +0200

    imported bare MUI EnhancedTable as SortTable

--- components/InvoiceList.js
+++ components/InvoiceList.js
@@ -1,8 +1,10 @@
+import SortTable from "./SortTable";
 export default function InvoiceList({ invoices, patients }) {
   return (
     <div>
       <h1>Invoice list</h1>
       <p>Found {invoices.length} invoices</p>
+      <SortTable />
     </div>
   );
 }
