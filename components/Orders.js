8b949939f1062f55f61e5a0071e86b83b0ba00e5
Marco De Canal <marco.decanal@gmail.com>
2022-04-28 15:26:44 +0200

    Table almost completed

--- components/Orders.js
+++ components/Orders.js
@@ -7,84 +7,57 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Title from "./Title";
 
-// Generate Order Data
-// function createData(id, date, name, shipTo, paymentMethod, amount) {
-//   return { id, date, name, shipTo, paymentMethod, amount };
-// }
-
-// const rows = [
-//   createData(
-//     0,
-//     "16 Mar, 2019",
-//     "Elvis Presley",
-//     "Tupelo, MS",
-//     "VISA ⠀•••• 3719",
-//     312.44
-//   ),
-//   createData(
-//     1,
-//     "16 Mar, 2019",
-//     "Paul McCartney",
-//     "London, UK",
-//     "VISA ⠀•••• 2574",
-//     866.99
-//   ),
-//   createData(
-//     2,
-//     "16 Mar, 2019",
-//     "Tom Scholz",
-//     "Boston, MA",
-//     "MC ⠀•••• 1253",
-//     100.81
-//   ),
-//   createData(
-//     3,
-//     "16 Mar, 2019",
-//     "Michael Jackson",
-//     "Gary, IN",
-//     "AMEX ⠀•••• 2000",
-//     654.39
-//   ),
-//   createData(
-//     4,
-//     "15 Mar, 2019",
-//     "Bruce Springsteen",
-//     "Long Branch, NJ",
-//     "VISA ⠀•••• 5919",
-//     212.79
-//   ),
-// ];
-
 function preventDefault(event) {
   event.preventDefault();
 }
 
 export default function Orders({ invoices, patients }) {
-  const rows = [];
+  const now = new Date();
+  const oneYearAgo = new Date();
+  oneYearAgo.setFullYear(now.getFullYear()-1);
   const lastPatients = patients
     .sort((p1, p2) => {
       return Date.parse(p1.ultimaModifica) > Date.parse(p2.ultimaModifica) ? -1 : 1;
     })
-    .slice(0, 16);
+    .slice(0, 21);
+  const lastInvoices = invoices.filter(i => Date.parse(i.dataEmissione) > Date.parse(oneYearAgo))
+    .sort((i1, i2) => Date.parse(i1.dataEmissione) > Date.parse(i2.dataEmissione) ? -1 : 1);
+  const rows = lastPatients.map((p,i) => {
+    let amount = 0;
+    let lastInvoiceFound = undefined;
+    lastInvoices.forEach(i => {
+      if(i.paziente === p._id){
+        amount += Number.parseFloat(i.valore);
+        if (!lastInvoiceFound) lastInvoiceFound = Number.parseFloat(i.valore);
+      }
+    });
+    return {
+      id: i,
+      nome: `${p.nome} ${p.cognome}`,
+      data: new Date(p.ultimaModifica).toISOString(),
+      codFisc: p.codiceFiscale,
+      valore: `${lastInvoiceFound}€ (${amount}€)`
+    }
+  });
   return (
     <React.Fragment>
-      <Title>Recent Orders</Title>
+      <Title>Ultime fatture</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Data</TableCell>
             <TableCell>Nome</TableCell>
+            <TableCell>Data</TableCell>
             <TableCell>Codice fiscale</TableCell>
             <TableCell align="right">Valore (totale)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
+            <TableRow key={row.id} hover>
+              <TableCell>{row.nome}</TableCell>
+              <TableCell>{row.data}</TableCell>
+              <TableCell>{row.codFisc}</TableCell>
+              <TableCell align="right">{`${row.valore}`}</TableCell>
             </TableRow>
           ))}
         </TableBody>
