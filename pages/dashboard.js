5b2d67597337b811b5e9544ca9816bde15f9abe7
Marco De Canal <marco.decanal@gmail.com>
2022-05-05 22:32:08 +0200

    controller.js: getAllInvoices, and reading them from dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,6 +5,8 @@ import Layout from "../components/Layout";
 import React from "react";
 import Dashboard from "../components/Dashboard";
 import SignIn from "../components/SignIn";
+
+import { getAllInvoices } from "../lib/controller";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -17,14 +19,26 @@ import { patients } from "../lib/data/patients";
 export const getStaticProps = async () => {
   return {
     props: {
-      invoices,
+      // invoices,
       patients,
     },
   };
 };
 
-export default function DashboardPage({ invoices, patients }) {
-  const [loading, isLoading] = React.useState(true);
+export default function DashboardPage({ /*invoices,*/ patients }) {
+  const [isLoading, setLoading] = React.useState(true);
+  const [invoices, setInvoices] = React.useState(undefined);
+  getAllInvoices()
+    .then((res) => res.json())
+    .then((res) => {
+      setInvoices(res.invoices);
+      console.log(res);
+    })
+    .finally(() => setLoading(false))
+    .catch((err) => {
+      setInvoices(invoices);
+      console.error(err);
+    });
   return (
     <div>
       <Head>
@@ -35,7 +49,11 @@ export default function DashboardPage({ invoices, patients }) {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Dashboard invoices={invoices} patients={patients}></Dashboard>
+      {!isLoading ? (
+        <Dashboard invoices={invoices} patients={patients}></Dashboard>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
