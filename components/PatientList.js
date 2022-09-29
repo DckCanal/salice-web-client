54627b92be7ccdb547a5f837c4f6095ac35316f7
Marco De Canal <marco.decanal@gmail.com>
2022-09-29 20:17:53 +0200

    PatientList title set to Pazienti not nutritrion

--- components/PatientList.js
+++ components/PatientList.js
@@ -241,7 +241,7 @@ const EnhancedTableToolbar = (props) => {
           id="tableTitle"
           component="div"
         >
-          Nutrition
+          Pazienti
         </Typography>
       )}
 
