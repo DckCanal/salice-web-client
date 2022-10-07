fcd3e445e75398bfdc6c017fbc7fc206afb8a0de
Marco De Canal <marco.decanal@gmail.com>
2022-10-07 10:27:16 +0200

    patient detail personal info completed

--- components/PatientDetail.js
+++ components/PatientDetail.js
@@ -1,26 +1,96 @@
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+
 export default function PatientDetail({
   patient,
   invoices,
   openInvoiceDetail,
 }) {
   // invoices: is a subset of appData.invoices, containing only patient's ones.
+  const res = patient.indirizzoResidenza;
   return (
-    <Paper sx={{ m: 2 }}>
-      <p>
-        {/* Nome: {patient.nome} {patient.cognome} */}
-        Patient detail: {JSON.stringify(patient)}
-        {/* {invoices} */}
-      </p>
-      <Box>
-        {invoices.map((i) => (
-          <Typography key={i._id}>
-            `${i.paziente} == ${patient._id}`
-          </Typography>
-        ))}
+    <Paper sx={{ mt: 2, p: 4, maxWidth: "900px", mr: "auto", ml: "auto" }}>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="h5">
+          {patient.cognome.toUpperCase()} {patient.nome}
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "flex-end",
+            flexDirection: "column",
+          }}
+        >
+          {patient.ultimaModifica && (
+            <Typography variant="overline">
+              Ultima fattura{" "}
+              {new Date(patient.ultimaModifica).toLocaleDateString()}
+            </Typography>
+          )}
+          <Typography variant="overline">Prezzo {patient.prezzo}€</Typography>
+        </Box>
       </Box>
+      <Divider />
+      <Box sx={{ m: 2, ml: 2 }}>
+        <Typography variant="h6">Residenza</Typography>
+        <Box sx={{ ml: 2, mt: 1 }}>
+          {res && res.paese && res.provincia && res.cap && (
+            <Typography variant="body1">
+              {res.paese} ({res.provincia}), {res.cap}
+            </Typography>
+          )}
+          {res.via && res.civico && (
+            <Typography variant="body1">
+              {res.via} {res.civico}
+            </Typography>
+          )}
+        </Box>
+      </Box>
+      <Divider />
+      <Box sx={{ m: 2, ml: 2 }}>
+        <Typography variant="h6">Anagrafica</Typography>
+        <Box sx={{ ml: 2, mt: 1 }}>
+          {patient.codiceFiscale && (
+            <Typography variant="body1">
+              Codice fiscale: {patient.codiceFiscale}
+            </Typography>
+          )}
+          {patient.partitaIva && (
+            <Typography variant="body1">
+              Partita IVA: {patient.partitaIva}
+            </Typography>
+          )}
+          {patient.dataNascita && (
+            <Typography variant="body1">
+              Nato/a il {new Date(patient.dataNascita).toLocaleDateString()}
+              {patient.luogoNascita.paese && ` a ${patient.luogoNascita.paese}`}
+              {patient.luogoNascita.provincia &&
+                ` (${patient.luogoNascita.provincia})`}
+              {patient.luogoNascita.CAP && `, ${patient.luogoNascita.CAP}`}
+            </Typography>
+          )}
+        </Box>
+      </Box>
+      <Divider />
+      {(patient.email || patient.telefono) && (
+        <>
+          <Box sx={{ m: 2, ml: 2 }}>
+            <Typography variant="h6">Recapiti</Typography>
+            <Box sx={{ ml: 2, mt: 1 }}>
+              {patient.telefono && (
+                <Typography variant="body1">{patient.telefono}</Typography>
+              )}
+              {patient.email && (
+                <Typography variant="body1">{patient.email}</Typography>
+              )}
+            </Box>
+          </Box>
+          <Divider />
+        </>
+      )}
     </Paper>
   );
 }
