f222e6281c3cc153783ddb5a7bc3be79222f4558
Marco De Canal <marco.decanal@gmail.com>
2022-10-14 10:05:06 +0200

    SearchField component

--- components/SearchField.js
+++ components/SearchField.js
@@ -1,8 +1,63 @@
-import { TextField, Autocomplete, InputAdornment } from "@mui/material";
-import { Search } from "@mui/icons-material";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import { styled, alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
 
-export default function SearchField({ optionList }) {
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+// const SearchBox = styled(Box)(({ theme }) => ({
+//   borderRadius: theme.shape.borderRadius,
+//   backgroundColor: alpha(theme.palette.common.white, 0.15),
+//   "&:hover": {
+//     backgroundColor: alpha(theme.palette.common.white, 0.25),
+//   },
+// }));
+
+export default function SearchField({ optionList, openPatientDetail }) {
   return (
+    // <SearchBox>
     <Autocomplete
       disablePortal
       clearOnEscape
@@ -12,11 +67,26 @@ export default function SearchField({ optionList }) {
       options={optionList}
       sx={{ width: 300 }}
       onChange={(ev, val) => {
-        return;
+        val != null && openPatientDetail(val._id);
       }}
       renderInput={(params) => (
-        <TextField variant="standard" label="Paziente" {...params} />
+        // <TextField
+        //   variant="standard"
+        //   label="Paziente"
+        //   autoFocus={true}
+        //   {...params}
+        // />
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Paziente"
+            inputProps={{ "aria-label": "Paziente" }}
+          />
+        </Search>
       )}
     />
+    // </SearchBox>
   );
 }
