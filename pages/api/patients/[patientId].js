2325bfcaec6b16d54c9f59f0dcf2da13fdbb4f27
Marco De Canal <marco.decanal@gmail.com>
2022-11-25 18:59:42 +0100

    patient API endpoint nightly

---
+++ pages/api/patients/[patientId].js
@@ -0,0 +1,97 @@
+import Invoice from "../../../models/invoiceModel";
+import Patient from "../../../models/patientModel";
+import { protectAndGetUser } from "../../../lib/protectRoute";
+import sendBadRequest from "../../../lib/badRequestError";
+import mongoose from "mongoose";
+import AppError from "../../../lib/appError";
+import filterObj from "../../../lib/filterObj";
+
+export default async function handler(req, res) {
+  const { method } = req;
+  if (!["GET", "PATCH", "DELETE"].includes(method)) {
+    sendBadRequest(res);
+    console.error(`${method} can't be managed`);
+    return;
+  }
+  try {
+    const user = await protectAndGetUser(req);
+    const { patientId } = req.query;
+    if (!mongoose.Types.ObjectId.isValid(patientId))
+      throw new AppError(`Id ${patientId} is not a valid id.`, 400);
+    const patient = await Patient.findById(patientId);
+    if (!patient || String(patient.utente) !== String(user._id))
+      throw new AppError(`Patient with id ${patientId} not found.`, 404);
+
+    if (method === "GET") {
+      res.status(200).json({
+        status: "success",
+        data: {
+          patient,
+        },
+      });
+    } else if (method === "PATCH") {
+      const filteredBody = filterObj(
+        req.body,
+        "nome",
+        "cognome",
+        "codiceFiscale",
+        "partitaIva",
+        "indirizzoResidenza",
+        "telefono",
+        "email",
+        "dataNascita",
+        "luogoNascita",
+        "prezzo",
+        "attivo"
+      );
+      // Filling structured fields
+      if (filteredBody.indirizzoResidenza !== undefined) {
+        Object.keys(patient.indirizzoResidenza).forEach((key) => {
+          if (filteredBody.indirizzoResidenza[key] == undefined)
+            filteredBody.indirizzoResidenza[key] =
+              patient.indirizzoResidenza[key];
+        });
+      }
+      if (filteredBody.luogoNascita !== undefined) {
+        console.log(typeof patient.luogoNascita);
+        Object.keys(patient.luogoNascita).forEach((key) => {
+          if (filteredBody.luogoNascita[key] == undefined)
+            filteredBody.luogoNascita[key] = patient.luogoNascita[key];
+        });
+      }
+      const updatedPatient = await Patient.findByIdAndUpdate(
+        patientId,
+        filteredBody,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+      if (!patiupdatedPatientent) {
+        throw new AppError(
+          `There was a problem while updating patient. Try again later`,
+          500
+        );
+      }
+      res.status(200).json({
+        status: "success",
+        data: {
+          patient: updatedPatient,
+        },
+      });
+    } else if (method === "DELETE") {
+      const unactivedPatient = await Patient.findByIdAndUpdate(patientId, {
+        attivo: false,
+      });
+      if (!unactivedPatient)
+        throw new AppError(`Patient not found. ID: ${patientId}`, 404);
+
+      res.status(204).json({
+        status: "success",
+        data: null,
+      });
+    }
+  } catch (err) {
+    res.status(err.statusCode).json({ message: err.message });
+  }
+}
---
+++ pages/api/patients/index.js
@@ -0,0 +1,93 @@
+import Invoice from "../../../models/invoiceModel";
+import Patient from "../../../models/patientModel";
+import { protectAndGetUser } from "../../../lib/protectRoute";
+import sendBadRequest from "../../../lib/badRequestError";
+import mongoose from "mongoose";
+import AppError from "../../../lib/appError";
+import filterObj from "../../../lib/filterObj";
+
+export default async function handler(req, res) {
+  const { method } = req;
+  if (method !== "GET" && method !== "POST") {
+    sendBadRequest(req, res);
+    console.error(`${method} can't be managed`);
+    return;
+  }
+  try {
+    const user = await protectAndGetUser(req);
+    if (method === "GET") {
+      const { sort, active } = req.query;
+      const page = req.query.page * 1 || 1;
+      const limit = req.query.limit * 1 || 5000;
+      const skip = (page - 1) * limit;
+      const sortParams =
+        sort !== undefined ? sort.split(",") : ["-cognome -nome"];
+      const allowedSort = [
+        "ultimaModifica",
+        "cognome",
+        "nome",
+        "dataNascita",
+        "prezzo",
+      ];
+      allowedSort.forEach((s) => {
+        allowedSort.push(`-${s}`);
+      });
+      const filteredSortParams = sortParams.filter((p) =>
+        allowedSort.includes(p)
+      );
+
+      let filter = {
+        utente: user._id,
+      };
+      const allowedActiveState = ["true", "false", "both"];
+      const activeState = allowedActiveState.includes(active) ? active : "true";
+      if (activeState === "true") filter["attivo"] = { $ne: false };
+      else if (activeState === "false") filter["attivo"] = { $eq: false };
+
+      const patients = await Patient.find(filter)
+        .sort(filteredSortParams.join(" "))
+        .skip(skip)
+        .limit(limit);
+      if (patients == undefined)
+        throw new AppError(
+          `There was an error retrieving patients... Please try again later.`,
+          500
+        );
+      res.status(200).json({
+        status: "success",
+        results: patients.length,
+        data: { patients },
+      });
+    } else if (method === "POST") {
+      const filteredBody = filterObj(
+        { ...req.body, utente: user._id },
+        "nome",
+        "cognome",
+        "codiceFiscale",
+        "partitaIva",
+        "indirizzoResidenza",
+        "telefono",
+        "email",
+        "dataNascita",
+        "luogoNascita",
+        "prezzo",
+        "ultimaModifica",
+        "utente"
+      );
+      const patient = await new Patient(filteredBody).save();
+      if (!patient)
+        throw new AppError(
+          `There was an error creating new patient... Try again later.`,
+          500
+        );
+      res.status(201).json({
+        status: "success",
+        data: {
+          patient,
+        },
+      });
+    }
+  } catch (err) {
+    res.status(err.statusCode).json({ message: err.message });
+  }
+}
