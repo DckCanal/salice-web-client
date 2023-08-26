65f26c88ad55295c12a42b57b796eda43bb43024
Marco De Canal <marco.decanal@gmail.com>
2023-08-26 16:29:55 +0200

    update invoice and patient pages

--- pages/invoices/update/[id].js
+++ pages/invoices/update/[id].js
@@ -1,10 +1,28 @@
 import { useRouter } from "next/router";
 import Layout from "../../../components/layout";
 import { useInvoice, usePatients } from "../../../lib/hooks";
+import UpdateInvoiceView from "../../../components/UpdateInvoiceView";
 
 export default function UpdateInvoicePage() {
   const router = useRouter();
   const id = router.query.id;
+  const {
+    invoice,
+    isLoading: invoiceLoading,
+    isError: invoiceError,
+  } = useInvoice(id);
+  const {
+    patients,
+    isLoading: patientsLoading,
+    isError: patientsError,
+  } = usePatients();
+
+  if (invoiceLoading || patientsLoading) return <p>Loading...</p>;
+  if (invoiceError || patientsError) return <p>Error loading invoice!</p>;
+  const patient = patients.find(
+    (p) => String(p._id) === String(invoice.paziente)
+  );
+  return <UpdateInvoiceView invoice={invoice} patient={patient} />;
 }
 
 UpdateInvoicePage.getLayout = function getLayout(page) {
--- pages/patients/update/[id].js
+++ pages/patients/update/[id].js
@@ -1,9 +1,15 @@
 import { useRouter } from "next/router";
 import Layout from "../../../components/layout";
+import UpdatePatientView from "../../../components/UpdatePatientView";
 import { usePatient } from "../../../lib/hooks";
 export default function UpdatePatientPage() {
   const router = useRouter();
   const id = router.query.id;
+  const { patient, isError, isLoading } = usePatient(id);
+  if (isLoading) return <p>Loading patient...</p>;
+  if (isError) return <p> Error loading patient! </p>;
+  if (patient) return <UpdatePatientView patient={patient} />;
+  else return <p>Something went wrong...</p>;
 }
 UpdatePatientPage.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
