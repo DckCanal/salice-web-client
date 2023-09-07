a71283dde2dec762902585af407f55d4491051e4
DckCanal <marco.decanal@gmail.com>
2023-09-07 13:51:41 +0000

    commenting unused code

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -8,200 +8,201 @@ import "@fontsource/roboto/700.css";
 
 import Layout from "../components/layout";
 import Home from "../components/Home";
-import { sortDate } from "../lib/dateUtils";
-import {
-  newInvoice,
-  newPatient,
-  updateInvoice,
-  updatePatient,
-  deleteInvoice,
-  deletePatient,
-} from "../lib/controller";
+
+// import { sortDate } from "../lib/dateUtils";
+// import {
+//   newInvoice,
+//   newPatient,
+//   updateInvoice,
+//   updatePatient,
+//   deleteInvoice,
+//   deletePatient,
+// } from "../lib/controller";
 
 export default function DashboardPage() {
-  const [appData, setAppData] = React.useState({
-    invoices: undefined,
-    patients: undefined,
-  });
+  // const [appData, setAppData] = React.useState({
+  //   invoices: undefined,
+  //   patients: undefined,
+  // });
 
-  const dataManager = {
-    addInvoice: async (
-      patientId,
-      cashed,
-      amount,
-      text,
-      issueDateTime,
-      d = false
-    ) => {
-      const response = await newInvoice(
-        patientId,
-        cashed,
-        amount,
-        text,
-        issueDateTime,
-        d
-      );
-      if (response.newInvoice) {
-        // OK, invoice created
-        const updatedPatient = appData.patients.find(
-          (p) => String(p._id) === String(response.newInvoice.paziente)
-        );
-        if (d === false) updatedPatient.ultimaModifica = new Date();
-        if (!d) {
-          updatedPatient.fatturatoUltimoAnno += Number.parseFloat(
-            response.newInvoice.valore
-          );
-        } else {
-          updatedPatient.dfatturatoUltimoAnno += Number.parseFloat(
-            response.newInvoice.valore
-          );
-        }
-        const invoices = [...appData.invoices, response.newInvoice].sort(
-          (invA, invB) => sortDate(invA.dataEmissione, invB.dataEmissione)
-        );
-        setAppData({
-          ...appData,
-          invoices,
-        });
-        return response.newInvoice;
-      } else {
-        // ERROR, TODO: manage error response
-        return response;
-      }
-    },
-    addPatient: async (
-      nome,
-      cognome,
-      codiceFiscale,
-      partitaIva,
-      paeseResidenza,
-      provinciaResidenza,
-      capResidenza,
-      viaResidenza,
-      civicoResidenza,
-      telefono,
-      email,
-      dataNascita,
-      paeseNascita,
-      provinciaNascita,
-      capNascita,
-      prezzo
-    ) => {
-      const response = await newPatient(
-        nome,
-        cognome,
-        codiceFiscale,
-        partitaIva,
-        paeseResidenza,
-        provinciaResidenza,
-        capResidenza,
-        viaResidenza,
-        civicoResidenza,
-        telefono,
-        email,
-        dataNascita,
-        paeseNascita,
-        provinciaNascita,
-        capNascita,
-        prezzo
-      );
-      if (response.newPatient) {
-        response.newPatient.fatturatoUltimoAnno = 0;
-        response.newPatient.dfatturatoUltimoAnno = 0;
-        response.newPatient.ultimaModifica = new Date();
-        setAppData({
-          ...appData,
-          patients: [response.newPatient, ...appData.patients],
-        });
-        return response.newPatient;
-      } else return response;
-    },
-    updateInvoice: async (invoiceId, newValues) => {
-      const response = await updateInvoice(invoiceId, newValues);
-      if (response.updatedInvoice) {
-        if (!response.updatedInvoice.d) {
-          setAppData({
-            ...appData,
-            invoices: [
-              ...appData.invoices.filter(
-                (i) => String(i._id) !== String(invoiceId)
-              ),
-              response.updatedInvoice,
-            ].sort((invA, invB) =>
-              sortDate(invA.dataEmissione, invB.dataEmissione)
-            ),
-          });
-        } else {
-          setAppData({
-            ...appData,
-            dinvoices: [
-              ...appData.dinvoices.filter(
-                (i) => String(i._id) !== String(invoiceId)
-              ),
-              response.updatedInvoice,
-            ].sort((invA, invB) =>
-              sortDate(invA.dataEmissione, invB.dataEmissione)
-            ),
-          });
-        }
-        return response.updatedInvoice;
-      } else return response;
-    },
-    removeInvoice: async (inv) => {
-      try {
-        const res = await deleteInvoice(inv._id);
-        if (res == true) {
-          setAppData({
-            ...appData,
-            invoices: [
-              ...appData.invoices.filter(
-                (i) => String(i._id) !== String(inv._id)
-              ),
-            ],
-          });
+  // const dataManager = {
+  //   addInvoice: async (
+  //     patientId,
+  //     cashed,
+  //     amount,
+  //     text,
+  //     issueDateTime,
+  //     d = false
+  //   ) => {
+  //     const response = await newInvoice(
+  //       patientId,
+  //       cashed,
+  //       amount,
+  //       text,
+  //       issueDateTime,
+  //       d
+  //     );
+  //     if (response.newInvoice) {
+  //       // OK, invoice created
+  //       const updatedPatient = appData.patients.find(
+  //         (p) => String(p._id) === String(response.newInvoice.paziente)
+  //       );
+  //       if (d === false) updatedPatient.ultimaModifica = new Date();
+  //       if (!d) {
+  //         updatedPatient.fatturatoUltimoAnno += Number.parseFloat(
+  //           response.newInvoice.valore
+  //         );
+  //       } else {
+  //         updatedPatient.dfatturatoUltimoAnno += Number.parseFloat(
+  //           response.newInvoice.valore
+  //         );
+  //       }
+  //       const invoices = [...appData.invoices, response.newInvoice].sort(
+  //         (invA, invB) => sortDate(invA.dataEmissione, invB.dataEmissione)
+  //       );
+  //       setAppData({
+  //         ...appData,
+  //         invoices,
+  //       });
+  //       return response.newInvoice;
+  //     } else {
+  //       // ERROR, TODO: manage error response
+  //       return response;
+  //     }
+  //   },
+  //   addPatient: async (
+  //     nome,
+  //     cognome,
+  //     codiceFiscale,
+  //     partitaIva,
+  //     paeseResidenza,
+  //     provinciaResidenza,
+  //     capResidenza,
+  //     viaResidenza,
+  //     civicoResidenza,
+  //     telefono,
+  //     email,
+  //     dataNascita,
+  //     paeseNascita,
+  //     provinciaNascita,
+  //     capNascita,
+  //     prezzo
+  //   ) => {
+  //     const response = await newPatient(
+  //       nome,
+  //       cognome,
+  //       codiceFiscale,
+  //       partitaIva,
+  //       paeseResidenza,
+  //       provinciaResidenza,
+  //       capResidenza,
+  //       viaResidenza,
+  //       civicoResidenza,
+  //       telefono,
+  //       email,
+  //       dataNascita,
+  //       paeseNascita,
+  //       provinciaNascita,
+  //       capNascita,
+  //       prezzo
+  //     );
+  //     if (response.newPatient) {
+  //       response.newPatient.fatturatoUltimoAnno = 0;
+  //       response.newPatient.dfatturatoUltimoAnno = 0;
+  //       response.newPatient.ultimaModifica = new Date();
+  //       setAppData({
+  //         ...appData,
+  //         patients: [response.newPatient, ...appData.patients],
+  //       });
+  //       return response.newPatient;
+  //     } else return response;
+  //   },
+  //   updateInvoice: async (invoiceId, newValues) => {
+  //     const response = await updateInvoice(invoiceId, newValues);
+  //     if (response.updatedInvoice) {
+  //       if (!response.updatedInvoice.d) {
+  //         setAppData({
+  //           ...appData,
+  //           invoices: [
+  //             ...appData.invoices.filter(
+  //               (i) => String(i._id) !== String(invoiceId)
+  //             ),
+  //             response.updatedInvoice,
+  //           ].sort((invA, invB) =>
+  //             sortDate(invA.dataEmissione, invB.dataEmissione)
+  //           ),
+  //         });
+  //       } else {
+  //         setAppData({
+  //           ...appData,
+  //           dinvoices: [
+  //             ...appData.dinvoices.filter(
+  //               (i) => String(i._id) !== String(invoiceId)
+  //             ),
+  //             response.updatedInvoice,
+  //           ].sort((invA, invB) =>
+  //             sortDate(invA.dataEmissione, invB.dataEmissione)
+  //           ),
+  //         });
+  //       }
+  //       return response.updatedInvoice;
+  //     } else return response;
+  //   },
+  //   removeInvoice: async (inv) => {
+  //     try {
+  //       const res = await deleteInvoice(inv._id);
+  //       if (res == true) {
+  //         setAppData({
+  //           ...appData,
+  //           invoices: [
+  //             ...appData.invoices.filter(
+  //               (i) => String(i._id) !== String(inv._id)
+  //             ),
+  //           ],
+  //         });
 
-          return true;
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    },
-    updatePatient: async (patientId, newValues) => {
-      const response = await updatePatient(patientId, newValues);
-      if (response.updatedPatient) {
-        setAppData({
-          ...appData,
-          patients: [
-            ...appData.patients.filter(
-              (p) => String(p._id) !== String(patientId)
-            ),
-            response.updatedPatient,
-          ].sort((invA, invB) =>
-            sortDate(invA.ultimaModifica, invB.ultimaModifica)
-          ),
-        });
-        return response.updatedPatient;
-      } else return response;
-    },
-    removePatient: async (p) => {
-      try {
-        const res = await deletePatient(p._id);
-        if (res == true) {
-          setAppData({
-            ...appData,
-            patients: [
-              ...appData.patients.filter(
-                (pat) => String(pat._id) !== String(p._id)
-              ),
-            ],
-          });
-          return true;
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    },
-  };
+  //         return true;
+  //       }
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   },
+  //   updatePatient: async (patientId, newValues) => {
+  //     const response = await updatePatient(patientId, newValues);
+  //     if (response.updatedPatient) {
+  //       setAppData({
+  //         ...appData,
+  //         patients: [
+  //           ...appData.patients.filter(
+  //             (p) => String(p._id) !== String(patientId)
+  //           ),
+  //           response.updatedPatient,
+  //         ].sort((invA, invB) =>
+  //           sortDate(invA.ultimaModifica, invB.ultimaModifica)
+  //         ),
+  //       });
+  //       return response.updatedPatient;
+  //     } else return response;
+  //   },
+  //   removePatient: async (p) => {
+  //     try {
+  //       const res = await deletePatient(p._id);
+  //       if (res == true) {
+  //         setAppData({
+  //           ...appData,
+  //           patients: [
+  //             ...appData.patients.filter(
+  //               (pat) => String(pat._id) !== String(p._id)
+  //             ),
+  //           ],
+  //         });
+  //         return true;
+  //       }
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   },
+  // };
 
   // const loadData = async () => {
   //   try {
